a73bec7bb5e2f8811c5663fc715efb5509ff025b
dangerulya <ardon232@mail.ru>
2025-04-04 16:41:44 +0300

    upload

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,45 @@
 // Copyright 2025 UNN-CS
 #include <cstdint>
+#include <cmath>
+
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) return false;
+  for (uint64_t i = 2; i <= std::sqrt(value); ++i) {
+    if (value % i == 0) return false;
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  uint64_t count = 0;
+  uint64_t num = 2;
+  while (count < n) {
+    if (checkPrime(num)) {
+      ++count;
+    }
+    ++num;
+  }
+  return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t num = value + 1;
+  while (true) {
+    if (checkPrime(num)) {
+      return num;
+    }
+    ++num;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; ++i) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,17 +1,91 @@
 // Copyright 2025 UNN-CS
-
 #include <gtest/gtest.h>
+
 #include <cstdint>
+
 #include "alg.h"
 
-
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(CheckPrimeTest, CheckPrime1) {
+  EXPECT_TRUE(checkPrime(2));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(CheckPrimeTest, CheckPrime2) {
+  EXPECT_TRUE(checkPrime(3));
+}
+
+TEST(CheckPrimeTest, CheckPrime3) {
+  EXPECT_FALSE(checkPrime(4));
+}
+
+TEST(CheckPrimeTest, CheckPrime4) {
+  EXPECT_TRUE(checkPrime(13));
+}
+
+TEST(CheckPrimeTest, CheckPrime5) {
+  EXPECT_FALSE(checkPrime(15));
+}
+
+TEST(NPrimeTest, NPrime1) {
+  EXPECT_EQ(nPrime(1), 2);
+}
+
+TEST(NPrimeTest, NPrime2) {
+  EXPECT_EQ(nPrime(2), 3);
+}
+
+TEST(NPrimeTest, NPrime3) {
+  EXPECT_EQ(nPrime(5), 11);
+}
+
+TEST(NPrimeTest, NPrime4) {
+  EXPECT_EQ(nPrime(10), 29);
+}
+
+TEST(NPrimeTest, NPrime5) {
+  EXPECT_EQ(nPrime(20), 71);
+}
+
+// Тесты для функции nextPrime
+TEST(NextPrimeTest, NextPrime1) {
+  EXPECT_EQ(nextPrime(4), 5);
+}
+
+TEST(NextPrimeTest, NextPrime2) {
+  EXPECT_EQ(nextPrime(11), 13);
+}
+
+TEST(NextPrimeTest, NextPrime3) {
+  EXPECT_EQ(nextPrime(20), 23);
+}
+
+TEST(NextPrimeTest, NextPrime4) {
+  EXPECT_EQ(nextPrime(100), 101);
+}
+
+TEST(NextPrimeTest, NextPrime5) {
+  EXPECT_EQ(nextPrime(1000), 1009);
+}
+
+TEST(SumPrimeTest, SumPrime1) {
+  EXPECT_EQ(sumPrime(10), 17);
+}
+
+TEST(SumPrimeTest, SumPrime2) {
+  EXPECT_EQ(sumPrime(20), 77);
+}
+
+TEST(SumPrimeTest, SumPrime3) {
+  EXPECT_EQ(sumPrime(30), 129);
+}
+
+TEST(SumPrimeTest, SumPrime4) {
+  EXPECT_EQ(sumPrime(50), 328);
+}
+
+TEST(SumPrimeTest, SumPrime5) {
+  EXPECT_EQ(sumPrime(100), 1060);
+}
+
+TEST(SumPrimeTest, SumPrime6) {
+  EXPECT_EQ(sumPrime(2000000), 142913828922);
 }
